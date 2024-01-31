4408e81babe10600f68f2074f435cebbdb3cf8e3
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-01-31 14:57:55 -0500

    375. Creando las rutas relacionadas a usuarios

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ const app = express();
 app.use( express.static('public') );
 
 // Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    });
-});
+// TODO: auth //crear, login, renew
+app.use('/api/auth', require('./routes/auth'));
+// TODO: CRUD: Eventos
+
 
 //escuchar peticiones
 app.listen(process.env.PORT, () => {
