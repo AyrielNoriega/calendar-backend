ff112f97195a1bf46d6b167192384c4c60d42c2f
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-01-31 14:34:52 -0500

    373. Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hola mundo s');
+const express = require('express');
+
+//crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    });
+});
+
+//escuchar peticiones
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
