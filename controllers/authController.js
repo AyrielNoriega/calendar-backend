50bccb641d09c1f995aaab00447c673f910fb090
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-02-01 18:52:54 -0500

    378. Express Validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,19 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
     const { name, email, password } = req.body;
-    res.json({
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return  res.status(400).json({
+                    ok: false,
+                    errors: errors.mapped()
+                });
+    }
+
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -13,6 +24,15 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return  res.status(400).json({
+                    ok: false,
+                    errors: errors.mapped()
+                });
+    }
 
     res.json({
         ok: true,
