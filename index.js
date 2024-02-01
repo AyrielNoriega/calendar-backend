7126bae246bfef9e5a770d1f977cad86dbd992fd
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-02-01 18:25:02 -0500

    Recuperar información de un posteo

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 // Directorio public
 app.use( express.static('public') );
 
+//lectura y parseo del body
+app.use( express.json() );
+
 // Rutas
 // TODO: auth //crear, login, renew
 app.use('/api/auth', require('./routes/auth'));
